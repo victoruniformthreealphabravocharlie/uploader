5dabdcec9c6d675e8a607bfd7510a96dcb0b4730
victoruniformthreealphabravocharlie <84901888+victoruniformthreealphabravocharlie@users.noreply.github.com>
2023-02-22 00:20:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,39 +1,27 @@
 from pyrogram import Client, filters
-import urllib.request
+import pytz
+from datetime import datetime
 
+# create a new Pyrogram client instance
 app = Client(
     "my_bot",
-    api_id=10816184,
-    api_hash="54fcbe09fa0ea55509bf88bd04a9aff0",
-    bot_token="6261956816:AAEToEEo4_aRRMeRi2qPnnTUoQJn3uYHnI8"
+    api_id=123456,
+    api_hash="0123456789abcdef0123456789abcdef",
+    bot_token="1234567890:ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 )
 
-@app.on_message(filters.private)
-def download_and_upload(bot, message):
-    # Check if message contains a URL
-    if message.entities and message.entities[0].type == "url":
-        url = message.text
-        file_ext = url.split(".")[-1]
-        allowed_extensions = ["mp3", "mp4", "mkv", "jpg", "doc", "pdf", "png", "jpeg"]
-        
-        # Check if URL points to a valid file
-        if file_ext in allowed_extensions:
-            try:
-                # Download file from URL
-                file_name, _ = urllib.request.urlretrieve(url)
-                
-                # Upload file to Telegram
-                bot.send_document(
-                    chat_id=message.chat.id,
-                    document=file_name,
-                    caption=f"Here's your {file_ext} file!"
-                )
-            except Exception as e:
-                print(f"Error downloading or uploading file: {e}")
-        else:
-            bot.send_message(
-                chat_id=message.chat.id,
-                text="Invalid file type. Only files with extensions: " + ", ".join(allowed_extensions) + " are allowed."
-            )
+# handler function for the /time command
+@app.on_message(filters.command("time"))
+def time_command_handler(client, message):
+    # get the current time in Kolkata, India
+    tz = pytz.timezone("Asia/Kolkata")
+    current_time = datetime.now(tz)
 
+    # format the time as 12-hour time
+    time_str = current_time.strftime("%I:%M %p")
+
+    # send the time back to the user
+    client.send_message(chat_id=message.chat.id, text=f"The current time in Kolkata is {time_str}")
+
+# start the bot
 app.run()
